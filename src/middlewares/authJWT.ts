dd5e03c861e2497b05d188d2ea55ef442870998a
OscarPlanas <casplanas7@gmail.com>
2023-01-27 01:03:38 +0100

    VerifyToken fixed

--- src/middlewares/authJWT.ts
+++ src/middlewares/authJWT.ts
@@ -11,7 +11,9 @@ const secretoJWT: string = 'NuestraClaveEA3';
 
 export async function verifyToken(req: Request, res: Response, next: NextFunction) {
     console.log('VerifyToken');
-    const token = req.header("x-auth-token");
+    const token = req.headers['x-access-token'] as string;
+    //const token = req.header("x-auth-token");
+    console.log(token);
     if (!token) {
         return res.status(403).json({ message: 'No token provided' });
     }
