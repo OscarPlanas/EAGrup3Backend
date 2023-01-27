9e8769ce810d9f53042507871448a25053247dad
MarioAlva <mario.antonio.alva@estudiantat.upc.edu>
2023-01-27 15:26:08 +0100

    Comments & follow works

--- src/controller/eventController.ts
+++ src/controller/eventController.ts
@@ -1,15 +1,22 @@
 import Event from '../model/Event';
 import Comment from '../model/Comment';
+import User from '../model/User';
 
 import { Request, Response } from 'express';
 
 const getall = async (req: Request, res: Response) => {
-    const events = await Event.find().populate('owner').populate('participants').populate('comments');
+    const events = await Event.find().populate('owner').populate('participants').populate({
+		path: 'comments',
+		populate: { path: 'owner' }
+	});
     res.json(events);
 }
 
 const getone = async (req: Request, res: Response) => {
-    const event = await Event.findById(req.params.id_event).populate('owner').populate('participants').populate('comments');
+    const event = await Event.findById(req.params.id_event).populate('participants').populate({
+		path: 'comments',
+		populate: { path: 'owner' }
+	});
     if (!event) {
         return res.status(404).send('The event does not exist');
     }
@@ -55,33 +62,27 @@ const addComment = async (req: Request, res: Response) => {
         if (err) {
             return res.status(500).send(err);
         }
-        event.update(
-            { _id: event._id },
-            { $push: { comments: comment._id } },
-        );
-        event.save();
-        res.status(200).json({ status: 'Comment saved' });
     });
+	event.updateOne({ $push: { comments: comment._id } }, (err: any) => {
+		event.save();
+		res.status(200).json({ status: 'Comment saved' });
+	});
 }
 
-/*const addParticipant = async (req: Request, res: Response) => {
+const addParticipant = async (req: Request, res: Response) => {
     const event = await Event.findById(req.params.id_event);
     if (!event) {
         return res.status(404).send('The event does not exist');
     }
-    const participants = new Comment(req.body);
-    await comment.save( (err: any) => {
-        if (err) {
-            return res.status(500).send(err);
-        }
-        event.update(
-            { _id: event._id },
-            { $push: { comments: comment._id } },
-        );
-        event.save();
-        res.status(200).json({ status: 'Comment saved' });
-    });
-}*/
+    const participant = await User.findById(req.body.id);
+	if (event.participants.includes(participant?._id!)) {
+		return res.status(404).send('The user is already a participant');
+	}
+    event.updateOne({ $push: { participants: participant?._id } }, (err: any) => {
+		event.save();
+		res.status(200).json({ status: 'Participant saved' });
+	});
+}
 
 const getComments = async (req: Request, res: Response) => {
     const event = await Event.findById(req.params.id_event).populate('comments');
@@ -157,5 +158,6 @@ export default {
     getComment,
     updateComment,
     deleteComment,
-    addEvent
+    addEvent,
+	addParticipant
 }
--- src/controller/userController.ts
+++ src/controller/userController.ts
@@ -26,7 +26,7 @@ const register = async (req: Request, res: Response) => {
 	const token = jwt.sign({ id: newUser._id }, secretoJWT, {
 		expiresIn: 60 * 60 * 24
 	});
-	res.status(200).json({ auth: true, token });
+	res.status(200).json({ auth: true, token, id: newUser._id });
 };
 
 /*const login = async (req: Request, res : Response) => {
